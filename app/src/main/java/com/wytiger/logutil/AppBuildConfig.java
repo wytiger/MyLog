9ee570812be058f89179f2c6b6b1aa99a2a5cd58
wytiger <wy123456>
2016-12-17 16:15:30 +0800

    添加配置文件

---
+++ app/src/main/java/com/wytiger/logutil/AppBuildConfig.java
@@ -0,0 +1,11 @@
+package com.wytiger.logutil;
+
+/**
+ * description:
+ * Created by wytiger on 2016-12-17.
+ */
+
+public class AppBuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final boolean RELEASE = Boolean.parseBoolean("fase");
+}
